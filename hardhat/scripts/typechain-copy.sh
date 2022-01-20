506d762addae6dff10d77d96c38b2a87f4bde189
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-01-20 13:07:12 -0800

    .

--- hardhat/scripts/typechain-copy.sh
+++ hardhat/scripts/typechain-copy.sh
@@ -2,5 +2,3 @@
 npx hardhat typechain
 [ -d ../frontend ] && cp -r typechain-types ../frontend/lib \
   && echo "Copied typechain types to frontend"
-[ -d ../backend ] && cp -r typechain-types ../backend \
-  && echo "Copied typechain types to backend"
