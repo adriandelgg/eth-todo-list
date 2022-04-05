0fb65ada314ab15b757cbf670ec082b9925c9def
Adrian Delgado <adriandelgadoc1@gmail.com>
2022-04-04 17:13:50 -0700

    added

--- hardhat/test/TodoList.test.ts
+++ hardhat/test/TodoList.test.ts
@@ -1,9 +1,9 @@
-import { expect } from "chai";
-import { ethers } from "hardhat";
-import { SignerWithAddress } from "@nomiclabs/hardhat-ethers/signers";
-import { TodoList__factory, TodoList } from "../typechain-types/index";
+import { expect } from 'chai';
+import { ethers } from 'hardhat';
+import { SignerWithAddress } from '@nomiclabs/hardhat-ethers/signers';
+import { TodoList__factory, TodoList } from '../typechain-types/index';
 
-describe("TodoList", function () {
+describe('TodoList', function () {
 	let owner: SignerWithAddress,
 		bob: SignerWithAddress,
 		contract: TodoList,
@@ -12,31 +12,31 @@ describe("TodoList", function () {
 	beforeEach(async function () {
 		[owner, bob] = await ethers.getSigners();
 		const ContractFactory = new TodoList__factory(owner);
-		contract = await ContractFactory.deploy("Task 0");
+		contract = await ContractFactory.deploy('Task 0');
 		contract2 = contract.connect(bob);
 	});
 
-	it("should create a task in constructor", async () => {
+	it('should create a task in constructor', async () => {
 		const { content } = await contract.tasks(0);
-		expect(content).to.equal("Task 0");
+		expect(content).to.equal('Task 0');
 	});
 
-	it("should create a new task", async () => {
-		await contract.createTask("Task 1");
+	it('should create a new task', async () => {
+		await contract.createTask('Task 1');
 		const { content, id } = await contract.tasks(1);
 
-		expect(content).to.equal("Task 1");
+		expect(content).to.equal('Task 1');
 		expect(id).to.equal(1);
 	});
 
-	it("should complete task", async () => {
+	it('should complete task', async () => {
 		await contract.completeTask(0);
 		const { completed } = await contract.tasks(0);
 		expect(completed).to.be.true;
 	});
 
-	it("should remove task", async () => {
-		await contract.createTask("Task 1");
+	it('should remove task', async () => {
+		await contract.createTask('Task 1');
 		await contract.removeTask(1);
 
 		const { content, id } = await contract.tasks(1);
